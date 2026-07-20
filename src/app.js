e8a3c93281a70a9c22cf0fd81afe69099d703e63
EnzoThiourt <119426636+EnzoThiourt@users.noreply.github.com>
2026-07-20 23:40:34 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,4 @@
-const API_URL =
-  window.location.hostname === "localhost" ||
-  window.location.hostname === "127.0.0.1"
+const API_URL = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
     ? "http://localhost:3000"
     : "https://api.swordmanager.cloud";
 
